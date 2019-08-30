d34d8bfa9216681ffb6f80ee0e3cccaf6b90f077
89081314528 <89081314528@mail.ru>
2019-08-30 21:42:23 +0300

    30/08 создала метод с суммой

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,13 +2,16 @@ package com.company;
 /**метод, который считает сумму
  *метод, который считает разность
  * метод, который считает произведение
- * посчитать сумму 4949494 и 49494
+ * посчитать сумму 4949494 и 49494 и распечатать
  * посчитать разность 9299292 и 92929
  * посчитать произведение 93993939 и 9399393
  */
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(sum(4949494, 49494));
+    }
+    public static int sum(int a, int b) {
+        return a + b;
     }
 }
